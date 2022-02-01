b171b8889bc3b7bce5945e191a91cd131735f1d4
Thom Bruce <thom@thombruce.com>
2022-02-01 06:32:01 +0000

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,46 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'CandyFloss',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@rockband/nuxt',
+    ['@candyfloss/nuxt', { provider: 'https://mainnet.infura.io/v3/a185cad5258f4c89a0107035403eaa4e' }]
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/_address.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+div
+  input.form-input.px-4.py-3.rounded-full(
+    type='number'
+    min='0'
+    step='.0005'
+    v-model='eth'
+    class='bg-gray-300 dark:bg-gray-800'
+  )
+  CandyFlossDonateButton.rounded-full.bg-pink-500(:to='address' :value='eth')
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      address: this.$route.params.address,
+      eth: '.005'
+    }
+  }
+}
+</script>
